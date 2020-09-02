1275a3dee1d60f63eb93236a131d94b17ca08e79
Benjamin T. Carter <benjamin.t.carter@gmail.com>
2020-09-02 11:29:20 -0600

    eda file rename

---
+++ analyses/eda.R
@@ -0,0 +1,31 @@
+# PREAMBLE
+# Author: Benjamin T. Carter, PhD
+# Objective:
+
+# ENVIRONMENT ####
+# packages
+library(dplyr)
+library(icd) # for reading in ICD10 classifying information and calculating risk scores
+library(ggplot2)
+
+# paths
+data.dir.path <- file.path("C:","Users","CarteB","BILLINGS CLINIC", 
+                           "CSI & David Hedges - Same Day Care Project", "data")
+
+out.dir.path <- file.path("C:","Users","CarteB","BILLINGS CLINIC", 
+                          "CSI & David Hedges - Same Day Care Project", "data")
+
+# data
+
+df.flat.name <- file.path(data.dir.path, "sdc.flat.csv")
+
+df.flat <- read.csv(df.flat.name)
+
+ICD <- icd10cm2019
+
+# variables
+
+
+
+
+# ANALYSIS ####
